ef940df3fe426f55a20d802b2744eb47551d895a
Sean <>
2024-03-09 15:21:34 +0000

    show trees

--- trees/script.js
+++ trees/script.js
@@ -1,3 +1,11 @@
 import {trees} from '../data/trees.js';
 
-console.log (trees)
+const tree_list = document.getElementById('tree_list');
+
+// tree_list
+
+trees.forEach(tree => {
+	const tree_item = document.createElement('li');
+	tree_item.textContent = tree.title;
+	tree_list.appendChild(tree_item);
+})
