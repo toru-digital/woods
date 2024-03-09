b96099ccd29575dd65233df06a0214eec66539f3
Sean <>
2024-03-09 22:11:53 +0000

    delete old stuff

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div>HELLO WORLD</div>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Explore</a></li>
+    <li><a href="#">Map</a></li>
+    <li><a href="#">Trees</a></li>
+  </ul>
 </template>
