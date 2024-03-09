72f305762b8971ce44c767b97c0358a2be965e89
Sean <>
2024-03-09 15:44:07 +0000

    link and photo

--- data/trees.js
+++ data/trees.js
@@ -1,20 +1,9 @@
 export const trees = [
 	{
-		"title" : "Oak"
+		"title" : "Mulberry Tree",
+		"inaturalist_observation_id" : 140281316,
+		"lat" : "52.224408",
+		"lon" : "-0.890492",
+		"img" : "https://static.inaturalist.org/photos/240105711/small.jpg"
 	},
-	{
-		"title" : "Spruce"
-	},
-	{
-		"title" : "Birch"
-	},
-	{
-		"title" : "Jungle"
-	},
-	{
-		"title" : "Acacia"
-	},
-	{
-		"title" : "Dark Oak"
-	}
 ]
--- trees/script.js
+++ trees/script.js
@@ -1,11 +1,22 @@
 import {trees} from '../data/trees.js';
 
 const tree_list = document.getElementById('tree_list');
-
-// tree_list
+const prefix = "https://www.inaturalist.org/observations/"
 
 trees.forEach(tree => {
-	const tree_item = document.createElement('li');
-	tree_item.textContent = tree.title;
+	const tree_item = document.createElement('li')
+	tree_item.textContent = tree.title + " "
+
+	const a = document.createElement('a')
+	a.setAttribute ('href', prefix + tree.inaturalist_observation_id)
+	a.setAttribute ('target', "_blank")
+	a.textContent = 'iNaturalist'
+	tree_item.appendChild(a)
+
+	const img = document.createElement('img')
+	img.setAttribute ('src', tree.img)
+	img.setAttribute ('alt', tree.title)
+	tree_item.appendChild(img)
+
 	tree_list.appendChild(tree_item);
 })
