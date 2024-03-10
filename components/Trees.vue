77c1161f792e99bad19339c09037afa90c3ef27a
Sean <>
2024-03-10 10:52:03 +0000

    show my position

--- components/Trees.vue
+++ components/Trees.vue
@@ -1,5 +1,6 @@
 <script setup>
-const prefix = "https://www.inaturalist.org/observations/";
+import { ref, reactive } from "vue";
+
 const trees = [
 	{
 		title: "Mulberry Tree",
@@ -9,9 +10,33 @@ const trees = [
 		img: "https://static.inaturalist.org/photos/240105711/small.jpg",
 	},
 ];
+
+const user_data = reactive({
+	lat: 0,
+	lon: 0,
+});
+
+const onReceiveLocation = (position) => {
+	user_data.lat = position.coords.latitude;
+	user_data.lon = position.coords.longitude;
+};
+
+const updateMyPosition = function () {
+	if (!process.client || !navigator.geolocation) return;
+	navigator.geolocation.getCurrentPosition(onReceiveLocation);
+};
 </script>
 
 <template>
+	<div class="container my-12 mx-auto px-4 md:px-12">
+		<button
+			class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+			@click="updateMyPosition"
+		>
+			Update My Position
+		</button>
+		<h2>Your position: {{ user_data.lat }}, {{ user_data.lon }}</h2>
+	</div>
 	<div class="container my-12 mx-auto px-4 md:px-12">
 		<div class="flex flex-wrap -mx-1 lg:-mx-4">
 			<Card
@@ -52,21 +77,3 @@ const trees = [
 		</div>
 	</div>
 </template>
-
-<!-- <template>
-  <ul>
-    <li>
-      <h3>{{ tree.title }}</h3>
-      <a
-        
-        
-      >
-        View on 
-      </a>
-      <br />
-      
-      <br />
-      <img :src="tree.img" />
-    </li>
-  </ul>
-</template> -->
