46926e8bdd53cb1d6d4e9aa8205ee5b87840e7d0
Sean <>
2024-03-10 11:13:32 +0000

    static target

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   ssr: false,
+  target: 'static',
   postcss: {
     plugins: {
       tailwindcss: {},
