76f900adf67ac510173f9b35f6dff027fb32782b
Sean <>
2024-03-10 10:55:44 +0000

    show distance

--- components/Trees.vue
+++ components/Trees.vue
@@ -25,6 +25,28 @@ const updateMyPosition = function () {
 	if (!process.client || !navigator.geolocation) return;
 	navigator.geolocation.getCurrentPosition(onReceiveLocation);
 };
+
+const deg2rad = (degrees) => {
+	var pi = Math.PI;
+	return degrees * (pi / 180);
+};
+
+const getDistance = (lat1, lon1, lat2, lon2) => {
+	if (lat2 == 0 && lon2 == 0) return "...";
+
+	const R = 6371; // Radius of the earth in km
+	const dLat = deg2rad(lat2 - lat1); // deg2rad below
+	const dLon = deg2rad(lon2 - lon1);
+	const a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(deg2rad(lat1)) *
+			Math.cos(deg2rad(lat2)) *
+			Math.sin(dLon / 2) *
+			Math.sin(dLon / 2);
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	const d = R * c; // Distance in km
+	return d.toFixed(2) + " km";
+};
 </script>
 
 <template>
@@ -43,7 +65,7 @@ const updateMyPosition = function () {
 				v-for="tree in trees"
 				:image="tree.img"
 				:title="tree.title"
-				:meta_1="'200m away'"
+				:meta_1="getDistance(tree.lat, tree.lon, user_data.lat, user_data.lon)"
 				:key="tree.inaturalist_observation_id"
 			>
 				<a
