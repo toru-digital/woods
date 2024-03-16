e871e5c953be6fdef9737b7314fd35a6699553a4
Sean <>
2024-03-16 13:27:35 +0000

    map

--- components/Map.vue
+++ components/Map.vue
@@ -1,23 +1,18 @@
+<script setup>
+import { ref } from "vue";
+const zoom = ref(18);
+</script>
+
 <template>
 	<div style="height: 100vh; width: 100vw">
-		<LMap ref="map" :zoom="zoom" :center="[47.21322, -1.559482]">
+		<LMap ref="map" :zoom="zoom" :center="[52.224723, -0.887954]">
 			<LTileLayer
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 				layer-type="base"
 				name="OpenStreetMap"
 			/>
+			<LMarker :lat-lng="[52.224723, -0.887954]"></LMarker>
 		</LMap>
 	</div>
 </template>
-
-<script setup>
-import { ref } from "vue";
-const zoom = ref(6);
-</script>
-
-<style>
-body {
-	margin: 0;
-}
-</style>
