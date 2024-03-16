7d540e5f1a188859dbac96e756717d81ee67db81
Sean <>
2024-03-16 13:08:21 +0000

    icons

--- components/MenuCard.vue
+++ components/MenuCard.vue
@@ -1,5 +1,10 @@
 <script setup>
-import { BeakerIcon } from "@heroicons/vue/24/solid";
+import {
+	MapIcon,
+	HomeIcon,
+	ListBulletIcon,
+	ArrowUpCircleIcon,
+} from "@heroicons/vue/24/solid";
 
 const props = defineProps({
 	title: {
@@ -11,20 +16,28 @@ const props = defineProps({
 		required: true,
 	},
 });
+
+const icons = {
+	Home: HomeIcon,
+	Explore: ArrowUpCircleIcon,
+	Map: MapIcon,
+	Trees: ListBulletIcon,
+};
 </script>
 
 <template>
 	<div
 		class="cursor-pointer w-full justify-center inline-block text-center pt-2 pb-1"
 	>
-		<BeakerIcon
+		<component
+			:is="icons[title]"
 			class="h-6 w-6 inline-block mb-1"
 			:class="{ 'text-blue-500': active, 'text-slate-400': !active }"
 		/>
 		<span
 			class="tab tab-home block text-xs"
 			:class="{ 'text-blue-500': active, 'text-slate-400': !active }"
-			>{{ title }}</span
+			>{{ title }} {{ icon }}</span
 		>
 	</div>
 </template>
