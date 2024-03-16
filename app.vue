b4ef1483fd50a689d4e9e9a6b9028384a3df618d
Sean <>
2024-03-16 11:58:05 +0000

    menu item

--- app.vue
+++ app.vue
@@ -4,6 +4,22 @@
 		<li><a href="#">Explore</a></li>
 		<li><a href="#">Map</a></li>
 		<li><a href="#">Trees</a></li>
-	</ul> -->
+	</ul>
 	<Trees />
+	 -->
+	<div class="w-full h-screen">
+		<!-- <section id="bottom-navigation" class="md:hidden block fixed inset-x-0 bottom-0 z-10 bg-white shadow"> // if shown only tablet/mobile-->
+		<section
+			id="bottom-navigation"
+			class="block fixed inset-x-0 bottom-0 z-10 bg-white shadow"
+		>
+			<div id="tabs" class="flex justify-between">
+				<MenuCard />
+				<MenuCard />
+				<MenuCard />
+				<MenuCard />
+				<MenuCard />
+			</div>
+		</section>
+	</div>
 </template>
