537247548ecebc18693eff5fce2cb086f3cb37cc
Sean <>
2024-03-17 12:36:26 +0000

    compass

--- components/Explore.vue
+++ components/Explore.vue
@@ -3,5 +3,6 @@ const props = defineProps({});
 </script>
 
 <template>
-	<h2 class="text-xl text-center mt-8">Explore</h2>
+	<h2 class="text-xl text-center mt-8 mb-8">Explore</h2>
+	<Compass />
 </template>
