f59d48f8e45767c7ba3946bf4f78e6df5f025d8e
Sean <>
2024-03-24 13:22:03 +0000

    grid style

--- components/trees/grid/Block.vue
+++ components/trees/grid/Block.vue
@@ -12,7 +12,14 @@ const props = defineProps({
 </script>
 
 <template>
-	<div class="w-full h-[100px] bg-slate-200 rounded-lg">
-		{{ title }}
+	<div
+		class="w-full h-[160px] bg-slate-200 rounded-lg bg-cover bg-center cursor-pointer flex items-end justify-center p-2"
+		:style="`background-image: url(` + image + `)`"
+	>
+		<div
+			class="font-bold text-center text-l text-white leading-none w-[min-content] bg-slate-900 p-2 bg-opacity-90 w-full"
+		>
+			{{ title }}
+		</div>
 	</div>
 </template>
