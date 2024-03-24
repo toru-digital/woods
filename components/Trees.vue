561da243a23c345d91df134b2913ebf387e2a1b9
Sean <>
2024-03-24 13:03:44 +0000

    tabs fixed

--- components/Trees.vue
+++ components/Trees.vue
@@ -13,21 +13,25 @@ const state = reactive({
 </script>
 
 <template>
-	<div class="border-b border-gray-200 dark:border-gray-700">
-		<ul
-			class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400"
-		>
-			<TreesTab
-				:title="screen.title"
-				:icon="screen.icon"
-				v-for="(screen, index) in screens"
-				@click="state.screen_index = index"
-				:active="index === state.screen_index"
-				:key="screen.title"
-			/>
-		</ul>
-	</div>
-	<TreesGrid v-if="state.screen_index == 0" />
-	<TreesMap v-if="state.screen_index == 1" />
-	<TreesNear v-if="state.screen_index == 2" />
+	<secion class="flex flex-col w-full h-full">
+		<div class="border-b border-gray-200 dark:border-gray-700 shrink-0">
+			<ul
+				class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400"
+			>
+				<TreesTab
+					:title="screen.title"
+					:icon="screen.icon"
+					v-for="(screen, index) in screens"
+					@click="state.screen_index = index"
+					:active="index === state.screen_index"
+					:key="screen.title"
+				/>
+			</ul>
+		</div>
+		<div class="shrink h-full overflow-y-auto">
+			<TreesGrid v-if="state.screen_index == 0" />
+			<TreesMap v-if="state.screen_index == 1" />
+			<TreesNear v-if="state.screen_index == 2" />
+		</div>
+	</secion>
 </template>
