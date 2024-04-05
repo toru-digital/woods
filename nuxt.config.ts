522b69c9b2cd66e7a1187f21313fecbb07f1995f
Chris Sadler <chris@toruinteractive.com>
2024-04-05 22:25:32 +0100

    added aliases, page animations and css links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import Path from 'path';
+
 export default defineNuxtConfig({
-  modules: ['nuxt3-leaflet', "@nuxt/image"],
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+	app: {
+		pageTransition: { name: 'page', mode: 'out-in' }
+	},
+	modules: ['nuxt3-leaflet', "@nuxt/image"],
+	devtools: { enabled: true },
+	css: [
+		'~/assets/css/main.css',
+		'~/assets/css/animations.css',
+	],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	alias: {
+		"@data": Path.resolve(__dirname, "./data"),
+		"@components": Path.resolve(__dirname, "./components"),
+		"@layouts": Path.resolve(__dirname, "./layouts"),
+		"@pages": Path.resolve(__dirname, "./pages"),
+		"@plugins": Path.resolve(__dirname, "./plugins"),
+		"@static": Path.resolve(__dirname, "./static"),
+		"@store": Path.resolve(__dirname, "./store"),
+		"@interfaces": Path.resolve(__dirname, "./interfaces"),
+	}
 })
