98034a062d8a657f0e9cca4c1d0e291d1ecba356
Chris Sadler <chris@toruinteractive.com>
2024-04-05 22:27:51 +0100

    added custom styles for font and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./components/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./app.vue",
+		"./error.vue",
+	],
+	theme: {
+		extend: {
+			colors: {
+				overflow: '#033336',
+				header: '#033336',
+				body: '#FFA883',
+				footer: '#033336',
+				white: '#FFFFFF',
+			},
+			fontSize: {
+				tiny: '0.5rem',
+			},
+		},
+	},
+	plugins: [],
 }
 
