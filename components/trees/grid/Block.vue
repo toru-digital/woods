829e24e9e00e8aa910d8fb4bf83ec6c44a61a705
Chris Sadler <chris@toruinteractive.com>
2024-04-05 22:24:33 +0100

    make grid blocks round

--- components/trees/grid/Block.vue
+++ components/trees/grid/Block.vue
@@ -12,14 +12,12 @@ const props = defineProps({
 </script>
 
 <template>
-	<div
-		class="w-full h-[160px] bg-slate-200 rounded-lg bg-cover bg-center cursor-pointer flex items-end justify-center p-2"
-		:style="`background-image: url(` + image + `)`"
-	>
-		<div
-			class="font-bold text-center text-l text-white leading-none w-[min-content] bg-slate-900 p-2 bg-opacity-90 w-full"
-		>
-			{{ title }}
+	<div @click='$emit ("clickedon")'>
+		<div class='w-full p-3'>
+			<div class='w-full aspect-square rounded-full bg-cover bg-center cursor-pointer' :style="`background-image: url(` + image + `)`">
+
+			</div>
 		</div>
+		<p class='text-tiny text-center font-sans'>{{ title }}</p>
 	</div>
 </template>
