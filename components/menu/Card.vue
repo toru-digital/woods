998dc564ea4aa07fbee820290961082cc20cb42b
Chris Sadler <chris@toruinteractive.com>
2024-04-05 22:24:14 +0100

    cleanup card component

--- components/menu/Card.vue
+++ components/menu/Card.vue
@@ -1,42 +1,34 @@
 <script setup>
-import {
-	MapIcon,
-	HomeIcon,
-	ListBulletIcon,
-	ArrowUpCircleIcon,
-} from "@heroicons/vue/24/solid";
-
-const props = defineProps({
-	title: {
-		type: String,
-		required: true,
-	},
-	icon: {
-		type: String,
-		required: true,
-	},
-	active: {
-		type: Boolean,
-		required: true,
-	},
-});
-
-const icons = {
-	Home: HomeIcon,
-	Explore: ArrowUpCircleIcon,
-	Map: MapIcon,
-	Trees: ListBulletIcon,
-};
+	const props = defineProps({
+		title: {
+			type: String,
+			required: true,
+		},
+		icon: {
+			type: String,
+			required: true,
+		},
+		path: {
+			type: String,
+			required: true,
+		},
+		active: {
+			type: Boolean,
+			required: true,
+		},
+	});
 </script>
 
 <template>
-	<div
-		class="bg-slate-100 cursor-pointer w-full justify-center inline-block text-center pt-3 pb-5"
-	>
-		<img
-			:src="`/icons/40dp/black/` + icon"
-			class="h-8 w-8 inline-block mb-1"
-			:class="{ 'opacity-100': active, 'opacity-30': !active }"
-		/>
-	</div>
+	<router-link :to="path" class='w-full h-full'>
+		<div
+			class="cursor-pointer w-full justify-center inline-block text-center pt-3 pb-5"
+		>
+			<img
+				:src="`/icons/40dp/white/` + icon"
+				class="h-8 w-8 inline-block mb-1"
+				:class="{ 'opacity-100': active, 'opacity-30': !active }"
+			/>
+		</div>
+	</router-link>
 </template>
