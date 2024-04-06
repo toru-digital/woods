cbe91de333cbf422fdf66586085ea960cb1586c0
Sean <>
2024-04-06 15:05:13 +0100

    add colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,20 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				overflow: '#033336',
-				header: '#033336',
-				body: '#FFA883',
-				footer: '#033336',
-				white: '#FFFFFF',
+				"forest-900" : "#0A2D2F",
+				"mint-400" : "#54B286",
+				"blaze-400" : "#FA7143",
+				"forest-600" : "#3B7276",
+				"mint-200" : "#81D6AE",
+				"blaze-200" : "#FB9F7C",
+				black : "#000000",
+				"grey-100" : "#F0EEEE",
+				white : "#FFFFFF",
+
+				overflow: '#0A2D2F',
+				header: '#0A2D2F',
+				body: '#FFFFFF',
+				footer: '#0A2D2F',
 			},
 			fontSize: {
 				tiny: '0.5rem',
