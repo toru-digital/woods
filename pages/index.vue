179e99737491935c997db3dd1c34ff563eee54d3
Sean <>
2024-04-06 15:15:31 +0100

    show colours

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
 	<transition name="fade">
-		<h1>home</h1>
+		<section>
+			<h1>home</h1>
+
+			<div class="flex gap-2 w-full">
+				<div class="border border-grey w-20 h-20 bg-forest-900"></div>
+				<div class="border border-grey w-20 h-20 bg-mint-400"></div>
+				<div class="border border-grey w-20 h-20 bg-blaze-400"></div>
+				<div class="border border-grey w-20 h-20 bg-forest-600"></div>
+				<div class="border border-grey w-20 h-20 bg-mint-200"></div>
+				<div class="border border-grey w-20 h-20 bg-blaze-200"></div>
+				<div class="border border-grey w-20 h-20 bg-black"></div>
+				<div class="border border-grey w-20 h-20 bg-grey-100"></div>
+				<div class="border border-grey w-20 h-20 bg-white"></div>
+			</div>
+		</section>
 	</transition>
 </template>
