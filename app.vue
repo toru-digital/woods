1dc0833bfb7e53c321615878234c6ae52371940c
Sean <>
2024-04-06 13:22:45 +0100

    add title and description

--- app.vue
+++ app.vue
@@ -1,6 +1,22 @@
+<script setup lang="ts">
+useSeoMeta({
+	title: "Delapre Tree Trails",
+	ogTitle: "Delapre Tree Trails",
+	description:
+		"Embark on a digital journey through the enchanting landscapes of Delapré Abbey with our interactive Tree Quest. Discover the rich tapestry of history and nature, explore ancient woodlands, and connect with the stories of each unique tree that graces this historic estate.",
+	ogDescription:
+		"Embark on a digital journey through the enchanting landscapes of Delapré Abbey with our interactive Tree Quest. Discover the rich tapestry of history and nature, explore ancient woodlands, and connect with the stories of each unique tree that graces this historic estate.",
+	// ogImage: "https://example.com/image.png",
+	// twitterCard: "summary_large_image",
+});
+</script>
+
 <template>
 	<NuxtLayout>
 		<NuxtPage />
-		<link href="https://fonts.cdnfonts.com/css/sigmar-one" rel="stylesheet">
+		<link
+			href="https://fonts.cdnfonts.com/css/sigmar-one"
+			rel="stylesheet"
+		/>
 	</NuxtLayout>
 </template>
