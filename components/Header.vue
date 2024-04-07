3e6f171e40c99e0a869032ad30a5791f3d5be7d9
Sean <>
2024-04-07 12:14:20 +0100

    header logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,10 @@
 <script setup></script>
 
 <template>
-	<section class="w-full h-full relative">
-		<div class="w-full absolute bottom-2 px-4 flex justify-between">
-			<img :src="'/icons/app-icon.svg'" class="h-[60%]" />
-		</div>
+	<section class="w-full h-full relative px-2 py-1 flex items-end">
+		<img
+			:src="'/images/theme/delapre-tree-trails-logo.png'"
+			class="h-5/6"
+		/>
 	</section>
 </template>
