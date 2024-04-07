2d922658d5a96690ab609bd027e25fbc3eb09368
Sean <>
2024-04-07 12:36:18 +0100

    fix iON display bug - menu hanging off bottom

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
-	<div class="w-screen h-screen flex flex-col bg-overflow">
-		<div class="header bg-header h-20">
+	<div class="w-screen h-[100dvh] flex flex-col bg-overflow">
+		<div class="header bg-header h-[80px]">
 			<Header />
 		</div>
 		<div
-			class="body bg-body max-w-screen max-h-[calc(100vh-10rem)] min-h-[calc(100vh-10rem)] overflow-y-scroll"
+			class="body bg-body max-w-screen overflow-y-scroll h-[calc(100vh-160px)]"
 		>
 			<slot />
 		</div>
-		<div class="footer bg-footer h-20 flex flex-row">
+		<div class="footer bg-footer h-[80px] flex flex-row">
 			<MenuCard
 				v-for="screen in menu"
 				:key="screen.title"
@@ -25,46 +25,48 @@
 </template>
 
 <script setup>
-	import menu from "@data/menu";
-	import { useRouter } from "vue-router";
+import menu from "@data/menu";
+import { useRouter } from "vue-router";
 
-	const route = useRoute();
-	const router = useRouter();
+const route = useRoute();
+const router = useRouter();
 
-	const isActive = (path) => {
-		if (route == null) {
-			return false;
-		}
-
-		if (path === "/" && route.fullPath === "/") {
-			return route.fullPath === path;
-		}
-
-		if (path === "/") {
-			return route.fullPath === path;
-		}
-
-		return route.fullPath.startsWith(path);
+const isActive = (path) => {
+	if (route == null) {
+		return false;
 	}
 
-	const enableFullscreen = () => {
-		document.body.webkitRequestFullscreen();
+	if (path === "/" && route.fullPath === "/") {
+		return route.fullPath === path;
+	}
 
-		if (!(
+	if (path === "/") {
+		return route.fullPath === path;
+	}
+
+	return route.fullPath.startsWith(path);
+};
+
+const enableFullscreen = () => {
+	document.body.webkitRequestFullscreen();
+
+	if (
+		!(
 			document.fullscreenElement ||
 			document.webkitFullscreenElement ||
-			document.mozFullScreenElement)
-		) {
-			router.push("/setup");
-		}
-	};
+			document.mozFullScreenElement
+		)
+	) {
+		router.push("/setup");
+	}
+};
 
-	onMounted(function () {
-		window.addEventListener("touchend", enableFullscreen);
-		document.body.webkitRequestFullscreen();
-	});
+onMounted(function () {
+	window.addEventListener("touchend", enableFullscreen);
+	document.body.webkitRequestFullscreen();
+});
 
-	onUnmounted(function () {
-		window.removeEventListener("touchend", enableFullscreen);
-	});
+onUnmounted(function () {
+	window.removeEventListener("touchend", enableFullscreen);
+});
 </script>
