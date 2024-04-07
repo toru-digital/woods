80e36092bcf7b2edabc5f10ca9ca6b051cb6bad7
Sean <>
2024-04-07 12:50:10 +0100

    add icons

--- components/menu/Card.vue
+++ components/menu/Card.vue
@@ -1,26 +1,26 @@
 <script setup>
-	const props = defineProps({
-		title: {
-			type: String,
-			required: true,
-		},
-		icon: {
-			type: String,
-			required: true,
-		},
-		path: {
-			type: String,
-			required: true,
-		},
-		active: {
-			type: Boolean,
-			required: true,
-		},
-	});
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	icon: {
+		type: String,
+		required: true,
+	},
+	path: {
+		type: String,
+		required: true,
+	},
+	active: {
+		type: Boolean,
+		required: true,
+	},
+});
 </script>
 
 <template>
-	<router-link :to="path" class='w-full h-full'>
+	<router-link :to="path" class="w-full h-full">
 		<div
 			class="cursor-pointer w-full justify-center inline-block text-center pt-3 pb-5"
 		>
