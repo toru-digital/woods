4ce17e5243a0e346e4ec4440babd13e40468d4e7
Sean <>
2024-04-07 11:50:24 +0100

    remoe account icon

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,9 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
 	<section class="w-full h-full relative">
 		<div class="w-full absolute bottom-2 px-4 flex justify-between">
 			<img :src="'/icons/app-icon.svg'" class="h-[60%]" />
-			<img :src="'/icons/account.svg'" class="w-8 aspect-square mx-2" />
 		</div>
 	</section>
 </template>
