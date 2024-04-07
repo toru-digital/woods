562d8c6f417b943bbe4ca3daa37021b039f82dd4
Sean <>
2024-04-07 14:26:31 +0100

    Hero Block

--- components/hero/block.vue
+++ components/hero/block.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 		type: String,
 		required: true,
 	},
-	subtitle: {
+	description: {
 		type: String,
 		required: true,
 	},
@@ -25,7 +25,7 @@ const props = defineProps({
 
 <template>
 	<section
-		class="min-h-[500px] bg-cover bg-left-bottom relative"
+		class="min-h-[500px] bg-cover bg-left-bottom relative flex items-end p-3"
 		:style="'background-image:url(' + image + ')'"
 	>
 		<img
@@ -37,5 +37,21 @@ const props = defineProps({
 			src="/images/hero-bottom-right.png"
 			class="w-[153px] absolute bottom-0 right-0"
 		/>
+
+		<div class="mr-20">
+			<h1
+				class="font-sig text-white text-4xl relative uppercase leading-9"
+			>
+				{{ title }}
+			</h1>
+			<p class="font-mont text-white text-sm mr-12 mt-2 font-light">
+				{{ description }}
+			</p>
+			<div
+				class="bg-blaze-400 rounded-full px-6 py-2 text-white font-sig uppercase mt-4 mb-2 inline-block text-xl cursor-pointer select-none"
+			>
+				{{ button_label }}
+			</div>
+		</div>
 	</section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
 	<transition name="fade">
 		<section>
-			<HeroBlock image="/images/hero-bg.jpg" />
+			<HeroBlock
+				image="/images/hero-bg.jpg"
+				title="Explore the Tree Trails of Delape"
+				description="An immersive journey through the green heart of Northampton."
+				button_label="Start a Trail"
+				button_route=""
+			/>
 			<p>ewk</p>
 		</section>
 	</transition>
