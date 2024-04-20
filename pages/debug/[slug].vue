0d8eac14e68ed2375531ae6cef56797ad8d296f4
Sean <>
2024-04-20 10:55:09 +0100

    show error

--- pages/debug/[slug].vue
+++ pages/debug/[slug].vue
@@ -148,6 +148,18 @@
 								KM
 							</td>
 						</tr>
+
+						<tr class="bg-white dark:bg-gray-800">
+							<th
+								scope="row"
+								class="px-6 py-4 font-medium text-gray-900 dark:text-white"
+							>
+								Last Permission Error
+							</th>
+							<td class="px-6 py-4">
+								{{ $store.state.setPermissionsLastError }}
+							</td>
+						</tr>
 					</tbody>
 				</table>
 			</div>
