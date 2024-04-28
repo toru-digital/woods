662233ce939fdf2e407bfe575e32999a7393c555
Sean <>
2024-04-28 10:39:05 +0100

    show debug props

--- pages/debug/[slug].vue
+++ pages/debug/[slug].vue
@@ -2,22 +2,93 @@
 	<section class="flex flex-col w-full h-full flex-col">
 		<div>
 			<div class="relative overflow-x-auto">
-				<table
-					class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
-				>
+				<table class="debug-table">
 					<tbody>
-						<tr
-							v-for="row in rows"
-							:key="row.title"
-							class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-						>
-							<th
-								scope="row"
-								class="px-6 py-4 font-medium text-gray-900 dark:text-white"
-							>
-								{{ row.label }}
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">title</th>
+							<td class="debug-td">{{ tree.title }}</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">slug</th>
+							<td class="debug-td">{{ tree.slug }}</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">tree latitude</th>
+							<td class="debug-td">{{ tree.lat }}</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">tree longitude</th>
+							<td class="debug-td">{{ tree.lon }}</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">your latitude</th>
+							<td class="debug-td">
+								{{ $store.state.latitude }}
+							</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">your longitude</th>
+							<td class="debug-td">
+								{{ $store.state.longitude }}
+							</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">Northern Axis</th>
+							<td class="debug-td">
+								{{ $store.state.northern_axis }}
+							</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">Your Bearing</th>
+							<td class="debug-td">
+								{{
+									calculateBearing(
+										store.state.latitude,
+										store.state.longitude,
+										tree.lat,
+										tree.lon
+									)
+								}}
+							</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">
+								Northern Axis - Your Bearing
 							</th>
-							<td class="px-6 py-4">{{ row.value }}</td>
+							<td class="debug-td">
+								{{
+									$store.state.northern_axis -
+									calculateBearing(
+										store.state.latitude,
+										store.state.longitude,
+										tree.lat,
+										tree.lon
+									)
+								}}
+							</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">
+								Distance to Tree
+							</th>
+							<td class="debug-td">
+								{{
+									getDistance(
+										$store.state.latitude,
+										$store.state.longitude,
+										tree.lat,
+										tree.lon
+									)
+								}}KM
+							</td>
+						</tr>
+						<tr class="debug-tr">
+							<th class="debug-th" scope="row">
+								Last Permission Errror
+							</th>
+							<td class="debug-td">
+								{{ $store.state.setPermissionsLastError }}
+							</td>
 						</tr>
 					</tbody>
 				</table>
@@ -35,73 +106,4 @@ const slug = route.params?.slug;
 const { data: tree } = await useFetch(`/api/tree/${slug}`, {
 	key: slug,
 });
-
-const getRows = function () {
-	console.log("tree");
-	return [];
-	const bearing = calculateBearing(
-		store.state.latitude,
-		store.state.longitude,
-		tree.lat,
-		tree.lon
-	);
-
-	const bearing_difference = store.state.northern_axis - bearing;
-
-	const distance_to_tree = getDistance(
-		store.state.latitude,
-		store.state.longitude,
-		tree.lat,
-		tree.lon
-	);
-
-	return [
-		{
-			label: "Title",
-			value: tree.title + "!!",
-		},
-		{
-			label: "Slug",
-			value: tree.slug + "ewwe",
-		},
-		{
-			label: "Tree Lat",
-			value: tree.lat,
-		},
-		{
-			label: "Tree Lon",
-			value: tree.lon,
-		},
-		{
-			label: "Your Latitude",
-			value: store.state.latitude,
-		},
-		{
-			label: "Your Longitude",
-			value: store.state.longitude,
-		},
-		{
-			label: "Northern Axis",
-			value: store.state.northern_axis,
-		},
-		{
-			label: "Your Bearing",
-			value: bearing,
-		},
-		{
-			label: "Northern - Your Bearing",
-			value: bearing_difference,
-		},
-		{
-			label: "Distance to Tree",
-			value: distance_to_tree + " KM",
-		},
-		{
-			label: "Last Permission Errror",
-			value: store.state.setPermissionsLastError,
-		},
-	];
-};
-
-const rows = reactive(getRows());
 </script>
