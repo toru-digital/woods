5523e847e41f494d7092023db1bafd33ee0c5ff3
Sean <>
2024-04-28 09:40:59 +0100

    get tree works

--- server/api/tree/[slug].js
+++ server/api/tree/[slug].js
@@ -1,12 +1,12 @@
-// import { trees } from "../../data/trees.js";
+import { trees } from "../../data/trees";
 
 export default defineEventHandler((event) => {
-	// const slug = getRouterParam(event, 'slug')
-	// const tree = trees.find (t => t.slug == slug)
+	const slug = getRouterParam(event, 'slug')
+	const tree = trees.find (t => t.slug == slug)
 
-	// if (tree === undefined) {
-		// throw createError({ statusCode: 404, statusMessage: 'Tree Not Found' })
-	// }
+	if (tree === undefined) {
+		throw createError({ statusCode: 404, statusMessage: 'Tree Not Found' })
+	}
 
-	return ['ew'];
+	return tree;
  })
