ff5be124a96824570b2bb89b96b9b76a954b1559
Sean <>
2024-05-03 15:47:06 +0100

    tidy up UI

--- components/tree/Finder.vue
+++ components/tree/Finder.vue
@@ -8,16 +8,16 @@ const props = defineProps({
 });
 
 const user_data = reactive({
-	log: "...",
+	error: "",
 	deg: 0,
+	is_initiated: false,
 });
 
 const isIOS = getIsIOS();
 const isAndroid = getIsAndroid();
-const testInterval = null;
+let testInterval = null;
 
 const startCompass = function () {
-	user_data.log = "START";
 	const testing = true;
 
 	if (isIOS) {
@@ -25,36 +25,37 @@ const startCompass = function () {
 			.then((response) => {
 				if (response === "granted") {
 					window.addEventListener("deviceorientation", handler, true);
+					user_data.is_initiated = true;
 				} else {
-					alert("has to be allowed!");
+					user_data.error = "Compass permission not granted";
 				}
 			})
-			.catch(() => alert("not supported"));
+			.catch(() => {
+				user_data.error = "Compass functionality not supported";
+			});
 	} else if (isAndroid) {
 		window.addEventListener("deviceorientationabsolute", handler, true);
+		user_data.is_initiated = true;
 	} else if (testing) {
 		testInterval = setInterval(rotate, 1000);
+		user_data.is_initiated = true;
 	}
 };
 
 const rotate = function () {
 	user_data.deg = (user_data.deg + 5) % 360;
-
-	user_data.log = Math.round(user_data.deg) + "°";
 };
 
 const handler = function (e) {
-	user_data.log = "@@@";
 	user_data.deg = user_data.deg =
 		e.webkitCompassHeading || Math.abs(e.alpha - 360);
-
-	user_data.log = Math.round(user_data.deg) + "°";
 };
 
 onUnmounted(function () {
 	window.removeEventListener("deviceorientation", handler, true);
 	window.removeEventListener("deviceorientationabsolute", handler, true);
 	clearInterval(testInterval);
+	user_data.is_initiated = false;
 });
 </script>
 
@@ -62,6 +63,7 @@ onUnmounted(function () {
 	<div class="compass">
 		<div
 			class="compass-circle"
+			v-if="user_data.is_initiated"
 			:style="{
 				transform: `translate(-50%, -50%) rotate(${-user_data.deg}deg)`,
 			}"
@@ -78,10 +80,7 @@ onUnmounted(function () {
 				}"
 			/>
 		</div>
-	</div>
-	<div class="mt-10">
-		<h2 class="text-center">log: {{ user_data.log }}</h2>
-		<div class="flex justify-center mt-8">
+		<div v-else class="compass-blank">
 			<button
 				class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
 				@click="startCompass"
@@ -90,19 +89,26 @@ onUnmounted(function () {
 			</button>
 		</div>
 	</div>
-	<p class="text-center text-bold text-2xl">
-		{{
-			tree == null
-				? "?"
-				: getDistance(
-						$store.state.latitude,
-						$store.state.longitude,
-						tree.lat,
-						tree.lon
-				  )
-		}}
-		KM to {{ tree.title }}
-	</p>
+	<div class="mt-6">
+		<p
+			class="text-center text-bold text-2xl text-slate-300"
+			v-if="tree == null || $store.state.latitude == 0"
+		>
+			...
+		</p>
+		<p class="text-center text-bold text-2xl" v-else>
+			{{
+				getDistance(
+					$store.state.latitude,
+					$store.state.longitude,
+					tree.lat,
+					tree.lon
+				)
+			}}KM to {{ tree.title }}
+		</p>
+
+		<p class="text-center text-red-500">{{ user_data.error }}&nbsp;</p>
+	</div>
 </template>
 
 <style scoped>
@@ -115,7 +121,8 @@ onUnmounted(function () {
 }
 
 .compass .compass-circle,
-.compass .compass-arrow {
+.compass .compass-arrow,
+.compass .compass-blank {
 	position: absolute;
 	width: 100%;
 	height: 100%;
@@ -130,6 +137,13 @@ onUnmounted(function () {
 
 .compass .compass-circle {
 	background-image: url(/images/compass.png);
+}
+
+.compass .compass-blank {
+	background-image: url(/images/compass-blank.png);
+	align-items: center;
+	justify-content: center;
+	display: flex;
 }
 
 .compass .compass-arrow {
