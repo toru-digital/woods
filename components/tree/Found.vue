0cfecd0d7ca24cd99f35adc96a5e302b63458981
Sean <>
2024-05-27 16:11:29 +0100

    found view

---
+++ components/tree/Found.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const router = useRouter();
+
+const props = defineProps({
+	tree: {
+		required: true,
+	},
+	back_link: {
+		required: true,
+	},
+	more_link: {
+		required: true,
+	},
+});
+</script>
+
+<template>
+	<div
+		class="w-full h-full bg-black relative bg-cover bg-center"
+		:style="'background-image:url(' + tree.img + ')'"
+	></div>
+</template>
+
+<style scoped></style>
---
+++ pages/trees/found/[slug].vue
@@ -0,0 +1,16 @@
+<template>
+	<TreeFound
+		v-if="tree != null"
+		:tree="tree"
+		back_link="/trees"
+		:more_link="'/trees/' + tree.slug"
+	/>
+</template>
+
+<script setup>
+const route = useRoute();
+const slug = route.params?.slug;
+const { data: tree } = await useFetch(`/api/tree/${slug}`, {
+	key: slug,
+});
+</script>
