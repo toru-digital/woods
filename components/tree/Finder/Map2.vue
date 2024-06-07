fba8cb08a3277c3136b3d96c191fdccb50b2c43d
Sean <>
2024-06-07 10:06:05 +0100

    blank components

---
+++ components/tree/Finder/Map2.vue
@@ -0,0 +1,192 @@
+<script setup>
+import { reactive, ref } from "vue";
+const delapre_position = [52.224723, -0.887954];
+const map = ref(null);
+const router = useRouter();
+
+const props = defineProps({
+	tree: {
+		required: true,
+	},
+	back_link: {
+		required: true,
+	},
+	debug_link: {
+		required: true,
+	},
+	found_link: {
+		required: true,
+	},
+});
+
+const user_data = reactive({
+	error: "",
+	deg: 0,
+	is_initiated: false,
+	log: "...",
+	lat: 0,
+	lon: 0,
+});
+
+const checkFound = (distance) => {
+	if (distance < 0.02) {
+		router.push(props.found_link);
+	}
+};
+
+const locationHandler = function (position) {
+	const { latitude, longitude } = position.coords;
+
+	const distance = getDistance(
+		delapre_position[0],
+		delapre_position[1],
+		latitude,
+		longitude
+	);
+
+	checkFound(distance);
+
+	user_data.lat = latitude;
+	user_data.lon = longitude;
+};
+
+const isIOS = getIsIOS();
+const isAndroid = getIsAndroid();
+let testInterval = null;
+let position_interval = null;
+
+const getMapCenter = () => {
+	if (user_data.lat == 0 && user_data.lon == 0) return delapre_position;
+	return [user_data.lat, user_data.lon];
+};
+
+onNuxtReady(async () => {
+	if (!process.client || !navigator.geolocation) return;
+
+	navigator.geolocation.getCurrentPosition(locationHandler);
+	position_interval = setInterval(() => {
+		navigator.geolocation.getCurrentPosition(locationHandler);
+		updateMap();
+	}, 1000);
+});
+
+const zoom = ref(15);
+
+const startCompass = function () {
+	const testing = true;
+
+	if (isIOS) {
+		DeviceOrientationEvent.requestPermission()
+			.then((response) => {
+				if (response === "granted") {
+					window.addEventListener("deviceorientation", handler, true);
+					user_data.is_initiated = true;
+				} else {
+					user_data.error = "Compass permission not granted";
+				}
+			})
+			.catch(() => {
+				user_data.error = "Compass functionality not supported";
+			});
+	} else if (isAndroid) {
+		window.addEventListener("deviceorientationabsolute", handler, true);
+		user_data.is_initiated = true;
+	} else if (testing) {
+		testInterval = setInterval(rotate, 1000);
+		user_data.is_initiated = true;
+	}
+};
+
+const rotate = function () {
+	user_data.deg = (user_data.deg + 5) % 360;
+};
+
+const handler = function (e) {
+	user_data.deg = user_data.deg =
+		e.webkitCompassHeading || Math.abs(e.alpha - 360);
+};
+
+const getMapBounds = () => {
+	const user_pos_set = user_data.lat != 0 && user_data.lon != 0;
+
+	const tree_pos = {
+		lat: props.tree.lat,
+		lon: props.tree.lon,
+	};
+
+	const user_pos = {
+		lat: user_pos_set ? user_data.lat : tree_pos.lat,
+		lon: user_pos_set ? user_data.lon : tree_pos.lon,
+	};
+
+	let top_left = [
+		Math.max(user_pos.lat, tree_pos.lat),
+		Math.max(user_pos.lon, tree_pos.lon),
+	];
+
+	const bottom_right = [
+		Math.min(user_pos.lat, tree_pos.lat),
+		Math.min(user_pos.lon, tree_pos.lon),
+	];
+
+	//force the markers to the botttom of the screen
+	const height = bottom_right[0] - top_left[0];
+	top_left[0] -= height * 2;
+
+	return [top_left, bottom_right];
+};
+
+const updateMap = () => {
+	map.value.leafletObject.fitBounds(getMapBounds());
+};
+
+onUnmounted(function () {
+	window.removeEventListener("deviceorientation", handler, true);
+	window.removeEventListener("deviceorientationabsolute", handler, true);
+	clearInterval(testInterval);
+	clearInterval(position_interval);
+
+	user_data.is_initiated = false;
+});
+</script>
+
+<template>
+	<LMap
+		id="map"
+		ref="map"
+		:center="getMapCenter()"
+		:bounds="getMapBounds()"
+		:zoom="zoom"
+		:v-if="user_data.lat != 0 && user_data.lon != 0"
+		:options="{ zoomControl: false, attributionControl: false }"
+	>
+		<LTileLayer
+			url="https://d1up0v8yxutj1v.cloudfront.net/{z}/{x}/{y}.png"
+			attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+			layer-type="base"
+			:max-zoom="18"
+			name="OpenStreetMap"
+		/>
+		<LMarker :lat-lng="[tree.lat, tree.lon]">
+			<LIcon icon-url="/icons/tree_icon.png" :icon-size="[35, 35]" />
+			<LPopup>
+				<div
+					class="cursor-pointer text-lg font-bold text-mont"
+					v-on:click="$emit('select-tree', tree.slug)"
+				>
+					<img
+						:src="tree.images[0].src"
+						class="w-32 h-32 object-cover rounded-lg"
+					/>
+					{{ tree.title }}
+				</div>
+			</LPopup>
+		</LMarker>
+		<LMarker
+			:if="user_data.lat != 0 && user_data.lon != 0"
+			:lat-lng="[user_data.lat, user_data.lon]"
+		>
+			<LIcon icon-url="/icons/user_icon.png" :icon-size="[35, 35]" />
+		</LMarker>
+	</LMap>
+</template>
