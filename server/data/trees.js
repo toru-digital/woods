85824476614b520eba75efa4c0f40ffcd8a2ed04
Sean <>
2024-06-07 13:10:31 +0100

    add test tree

--- server/data/trees.js
+++ server/data/trees.js
@@ -2,11 +2,13 @@
 import {default as t140281316} from "./trees/mulberry-140281316"
 import {default as t140191672} from "./trees/tulip-tree-140191672"
 import {default as t126346432} from "./trees/white-willow-126346432"
+import {default as testTreeRacecourse} from "./trees/test-tree-racecourse"
 
 const trees_raw = [
 	t140281316,
 	t140191672,
 	t126346432,
+	testTreeRacecourse
 ];
 
 export const trees = trees_raw.map(t => Object.assign({
---
+++ server/data/trees/test-tree-racecourse.js
@@ -0,0 +1,29 @@
+export default {
+	"inaturalist_observation_id": 123,
+	"scientific_name": "123",
+	"title": "TEST TREE RACECOURSE",
+	"slug" : "test-tree-racecourse",
+	"lat": 52.24761764770262,
+	"lon": -0.8921014671601424,
+	"images" : [
+		{
+			"src" : "https://static.inaturalist.org/photos/240105711/medium.jpg",
+			"tags" : ["Dec"]
+		},
+		{
+			"src" : "https://static.inaturalist.org/photos/240105711/medium.jpg",
+			"tags" : ["Apr"]
+		}
+	],
+	"content": [
+		{
+			"type": "text",
+			"title": "Description",
+			"content" : "The mulberry is a deciduous tree that is native to Asia, Africa and North America. It is a fast-growing tree that can reach a height of 10-15m. The leaves are heart-shaped and the fruit is a multiple fruit, 2-3cm long, that is dark purple when ripe. The fruit is sweet and edible and is used in pies, tarts, wines and cordials. The tree is also grown for its leaves, which are used to feed silkworms."
+		},
+		{
+			"type": "image",
+			"source" : "/images/temp-tree-2.jpg"
+		},
+	],
+}
